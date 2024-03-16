3d80182f91b9d082a5ba86c414fc634a8e8c1343
alek <lyudskoy@gmail.com>
2024-03-16 18:25:23 +0500

    added javascript shell

--- flake.nix
+++ flake.nix
@@ -1,41 +1,51 @@
 {
-  inputs = {};
+  inputs = {
 
-  outputs = inputs: {
-    templates = {
-      ts-pnpm = {
-        path = ./templates/ts-pnpm;
-        description = ''
-          Typescript + PNPM starter
-        '';
-        welcomText = ''
-          Run `pnpm init` or `pnpm create ...`
-        '';
-      };
-
-      ts-beth = {
-        path = ./templates/ts-beth;
-        description = ''
-          TS, Bun, Elysia, Tailwindcss, HTMX
-        '';
-        welcomText = ''
-          Run `bun init` or `bun create ...`
-        '';
-      };
-
-      ts-lit = {
-        path = ./templates/ts-lit;
-        description = ''
-          Typescript + Lit starter
-        '';
-      };
-
-      ts-angular = {
-        path = ./templates/ts-angular;
-        description = ''
-          Angular starter
-        '';
-      };
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.javascript = import ./shells/js/shell.nix { inherit pkgs; };
+
+      templates = {
+        ts-pnpm = {
+          path = ./templates/ts-pnpm;
+          description = ''
+            Typescript + PNPM starter
+          '';
+          welcomText = ''
+            Run `pnpm init` or `pnpm create ...`
+          '';
+        };
+
+        ts-beth = {
+          path = ./templates/ts-beth;
+          description = ''
+            TS, Bun, Elysia, Tailwindcss, HTMX
+          '';
+          welcomText = ''
+            Run `bun init` or `bun create ...`
+          '';
+        };
+
+        ts-lit = {
+          path = ./templates/ts-lit;
+          description = ''
+            Typescript + Lit starter
+          '';
+        };
+
+        ts-angular = {
+          path = ./templates/ts-angular;
+          description = ''
+            Angular starter
+          '';
+        };
+      };
+    });
 }
